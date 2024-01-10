c116382fb469670f2ef4e4f3b69fbd09c9c7c68d
jeanCr23 <135839377+jeanCr23@users.noreply.github.com>
2024-01-10 22:25:33 +0530

    first commit

---
+++ candlewick/src/products.js
@@ -0,0 +1,60 @@
+import product1 from "./assets/prod1.png";
+import product2 from "./assets/prod2.png";
+import product3 from "./assets/prod3.png";
+import product4 from "./assets/prod4.png";
+import product5 from "./assets/prod5.png";
+import product6 from "./assets/prod6.png";
+import product7 from "./assets/prod7.png";
+import product8 from "./assets/prod8.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Watermelon Lemonade",
+    price: 15,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Winter Wonderland",
+    price: 14,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Thousand Wishes",
+    price: 14,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Cinnamon Vanilla",
+    price: 12.5,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Raibow Waves",
+    price: 14,
+    productImage: product5,
+  },
+
+  {
+    id: 6,
+    productName: "Coconut Delight ",
+    price: 12,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Apple Orchard Lane",
+    price: 16.5,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Christmas Special ",
+    price: 17,
+    productImage: product8,
+  },
+];
